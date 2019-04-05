e4b48f3e81228ca37f06fad40dc27abc531fc380
bpelikan <bartoszpelikan@gmail.com>
2019-04-05 17:54:41 +0200

    State Data Changes

--- todo/src/App.js
+++ todo/src/App.js
@@ -9,6 +9,12 @@ export default class App extends Component {
       userName: "Adam"
     }
   }
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Adam" ? "Bob" : "Adam"
+    })
+  }
   
   render() {
     return (
@@ -16,6 +22,9 @@ export default class App extends Component {
         <h4 className="bg-primary text-white text-center p-2">
           { this.state.userName }'s To Do List
         </h4>
+        <button className="btn btn-primary m-2" onClick={ this.changeStateData }>
+          Change
+        </button>
       </div>
     )
   };
