b69b4ecb389e7e1ccd406fae865d243f61f3658c
bpelikan <bartoszpelikan@gmail.com>
2019-04-09 19:04:49 +0200

    Use the Local Storage API to store data locally

--- todo/src/App.js
+++ todo/src/App.js
@@ -23,7 +23,8 @@ export default class App extends Component {
 
   createNewTodo = (task) => {
     if (!this.state.todoItems.find(item => item.action === task)) {
-        this.setState({ todoItems: [...this.state.todoItems, { action: task, done: false }] });
+        this.setState({ todoItems: [...this.state.todoItems, { action: task, done: false }]},
+          () => localStorage.setItem("todos", JSON.stringify(this.state)));
     }
   }
   
@@ -34,6 +35,19 @@ export default class App extends Component {
   todoTableRows = (doneValue) => this.state.todoItems.filter(item => item.done === doneValue).map(item =>
     <TodoRow key={ item.action } item={ item } callback={ this.toggleTodo } />
   );
+
+  componentDidMount = () => {
+    let data = localStorage.getItem("todos");
+    this.setState(data != null ? JSON.parse(data) : {
+      userName: "Adam",
+      todoItems: [
+          { action: "Buy Flowers", done: false },
+          { action: "Get Shoes", done: false },
+          { action: "Collect Tickets", done: true },
+          { action: "Call Joe", done: false }],
+        showCompleted: true
+    });
+  }
 
   render = () =>
       <div>
