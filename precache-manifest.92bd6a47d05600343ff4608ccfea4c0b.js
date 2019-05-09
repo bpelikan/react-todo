e037b8118859b7ba2314c862ae77ed77b7194dc0
bpelikan <bartoszpelikan@gmail.com>
2019-05-09 20:00:29 +0200

    Updates

---
+++ precache-manifest.92bd6a47d05600343ff4608ccfea4c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "052fad23ca9f036fcf47",
+    "url": "/react-todo/static/js/runtime~main.79666361.js"
+  },
+  {
+    "revision": "900dd8eb40fbee5d11f0",
+    "url": "/react-todo/static/js/main.928dea57.chunk.js"
+  },
+  {
+    "revision": "94b620cbe1e9d9087c6a",
+    "url": "/react-todo/static/js/2.5ea9423d.chunk.js"
+  },
+  {
+    "revision": "900dd8eb40fbee5d11f0",
+    "url": "/react-todo/static/css/main.728912a8.chunk.css"
+  },
+  {
+    "revision": "94b620cbe1e9d9087c6a",
+    "url": "/react-todo/static/css/2.dd6b584c.chunk.css"
+  },
+  {
+    "revision": "772a6e1c83d710f5beba0b933b5e8c8f",
+    "url": "/react-todo/index.html"
+  }
+];
